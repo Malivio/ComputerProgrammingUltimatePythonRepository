fd42ea8eb9dc3ad6ba41ef52ff9686d52c5578b9
OB Sayalinh <26obsayalinh@usd266.com>
2023-12-01 10:10:58 -0600

    Finished 12/1/23

--- 06ListsAndLoops/Assignment/main.py
+++ 06ListsAndLoops/Assignment/main.py
@@ -29,13 +29,23 @@ def average_act_score(scores):
             i += 1
     return n/i
 
-def any_true(arr): return True in arr
+def all_true(arr): return not False in arr and len(arr) > 0
+
+def any_true(arr): return True in arr and len(arr) > 0
+
+def mostly_true(arr): return arr.count(True) > arr.count(False)
 
 def has_vowel(arr):
     for l in arr:
-        if l in ["a", 'i', 'e', 'o', 'u']:
+        if l.lower() in ["a", 'i', 'e', 'o', 'u']:
             return True
     return False
+
+def randomList():
+    n = []
+    for x in range(random.randint(0,9)):
+        n.append(bool(random.randint(0,1)))
+    return n
 
 def main():
     print("count_failing_grades")
@@ -65,13 +75,29 @@ def main():
         for j in range(5):
             n.append(random.randint(-20, 50))
         print(n, "=>", average_act_score(n)) 
+
+    print("all_true")
+    for iterate in range(5):
+        n = randomList()
+        print(n, "=>", all_true(n))
         
     print("any_true")
     for iterate in range(5):
-        n = [bool(random.randint(0,1)),bool(random.randint(0,1)),bool(random.randint(0,1))]
-        print(n, any_true(n))
+        n = randomList()
+        print(n, "=>", any_true(n))
 
+    print("mostly_true")
     for iterate in range(5):
+        n = randomList()
+        print(n, "=>", mostly_true(n))
 
+    numToLetter = {0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z'}
+    print("has_vowel")
+    for iterate in range(5):
+        letters = []
+        for x in range(5):
+            letters.append(numToLetter[random.randint(0,25)])
+        print(letters, '=>', has_vowel(letters))
+        
 
 main()
